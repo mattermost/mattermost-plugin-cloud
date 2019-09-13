d826c2adc402eff8a341de8d87805b6936deb3eb
Gabe Jackson <gabe@coffeepowered.co>
2019-09-13 10:24:50 -0400

    Remove debugging logic

--- server/api.go
+++ server/api.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"github.com/mattermost/mattermost-server/plugin"
@@ -32,7 +31,6 @@ func (p *Plugin) ServeHTTP(c *plugin.Context, w http.ResponseWriter, r *http.Req
 }
 
 func (p *Plugin) handleUserInstalls(w http.ResponseWriter, r *http.Request) {
-	p.API.LogWarn(fmt.Sprintf("%+v", r.Header))
 	userID := r.Header.Get("Mattermost-User-ID")
 	if userID == "" {
 		http.Error(w, "Not authorized", http.StatusUnauthorized)
@@ -41,13 +39,13 @@ func (p *Plugin) handleUserInstalls(w http.ResponseWriter, r *http.Request) {
 
 	installsForUser, err := p.getUpdatedInstallsForUser(userID)
 	if err != nil {
-		http.Error(w, "something happened", http.StatusForbidden)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
 		return
 	}
 
 	data, err := json.Marshal(installsForUser)
 	if err != nil {
-		http.Error(w, "something happened", http.StatusForbidden)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
 		return
 	}
 
