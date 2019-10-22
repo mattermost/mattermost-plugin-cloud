871f2dd236debda7214c556a30c44df04f93dd3a
Gabe Jackson <gabe@coffeepowered.co>
2019-10-22 12:41:19 -0400

    Add status command option to not show clusters
    Showing clusters with the status command is now optional.

--- server/command_status.go
+++ server/command_status.go
@@ -5,6 +5,7 @@ import (
 
 	cloud "github.com/mattermost/mattermost-cloud/model"
 	"github.com/mattermost/mattermost-server/model"
+	flag "github.com/spf13/pflag"
 )
 
 const (
@@ -19,16 +20,29 @@ const (
 `
 )
 
+func getStatusFlagSet() *flag.FlagSet {
+	statusFlagSet := flag.NewFlagSet("status", flag.ContinueOnError)
+	statusFlagSet.Bool("include-clusters", false, "Whether to get cluster status or not")
+
+	return statusFlagSet
+}
+
+func parseStatusArgs(args []string) (bool, error) {
+	statusFlagSet := getStatusFlagSet()
+	err := statusFlagSet.Parse(args)
+	if err != nil {
+		return false, err
+	}
+
+	return statusFlagSet.GetBool("include-clusters")
+}
+
 // The status command is primarily intended to help the team administrating the
 // cloud infrastructure, so we don't publish the command in the help info.
 func (p *Plugin) runStatusCommand(args []string, extra *model.CommandArgs) (*model.CommandResponse, bool, error) {
-	clusters, err := p.cloudClient.GetClusters(&cloud.GetClustersRequest{
-		Page:           0,
-		PerPage:        100,
-		IncludeDeleted: false,
-	})
+	includeClusters, err := parseStatusArgs(args)
 	if err != nil {
-		return nil, false, err
+		return nil, true, err
 	}
 
 	installations, err := p.cloudClient.GetInstallations(&cloud.GetInstallationsRequest{
@@ -40,15 +54,28 @@ func (p *Plugin) runStatusCommand(args []string, extra *model.CommandArgs) (*mod
 		return nil, false, err
 	}
 
-	status := clusterTableHeader
-	for _, cluster := range clusters {
-		status += fmt.Sprintf("| %s | %s | %s |", cluster.ID, cluster.Size, cluster.State)
+	status := installationTableHeader
+	for _, installation := range installations {
+		status += fmt.Sprintf("| %s | %s | %s | %s | %s |", installation.ID, installation.DNS, installation.Size, installation.Version, installation.State)
+	}
+
+	if !includeClusters {
+		return getCommandResponse(model.COMMAND_RESPONSE_TYPE_EPHEMERAL, status), false, nil
+	}
+
+	clusters, err := p.cloudClient.GetClusters(&cloud.GetClustersRequest{
+		Page:           0,
+		PerPage:        100,
+		IncludeDeleted: false,
+	})
+	if err != nil {
+		return nil, false, err
 	}
 
 	status += "\n"
-	status += installationTableHeader
-	for _, installation := range installations {
-		status += fmt.Sprintf("| %s | %s | %s | %s | %s |", installation.ID, installation.DNS, installation.Size, installation.Version, installation.State)
+	status += clusterTableHeader
+	for _, cluster := range clusters {
+		status += fmt.Sprintf("| %s | %s | %s |", cluster.ID, cluster.Size, cluster.State)
 	}
 
 	return getCommandResponse(model.COMMAND_RESPONSE_TYPE_EPHEMERAL, status), false, nil
--- webapp/src/manifest.js
+++ webapp/src/manifest.js
@@ -1,2 +1,2 @@
 export const id = 'com.mattermost.cloud';
-export const version = '0.1.4';
+export const version = '0.1.5';
