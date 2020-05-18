d48fb99d1e7c06a4ee491215aef0eef7ae9e7901
Joram Wilander <jwawilander@gmail.com>
2020-05-18 09:54:08 -0400

    Add ability to spin up installation with multitenant rds (#45)

--- server/configuration.go
+++ server/configuration.go
@@ -13,8 +13,9 @@ const (
 	licenseOptionE20 = "e20"
 	licenseOptionTE  = "te"
 
-	databaseOptionRDS      = "aws-rds"
-	databaseOptionOperator = "operator"
+	databaseOptionRDS         = "aws-rds"
+	databaseOptionOperator    = "operator"
+	databaseOptionMultitenant = "aws-multitenant-rds"
 
 	filestoreOptionS3       = "aws-s3"
 	filestoreOptionOperator = "operator"
