b1f3c84aae2f4f62fe9c5cc86238abc2dcfedb86
Ian Whitlock <ian.whitlock@mattermost.com>
2021-11-10 15:09:38 -0600

    Update webapp dependencies and fix webapp build

--- webapp/src/components/sidebar_right/sidebar_right.jsx
+++ webapp/src/components/sidebar_right/sidebar_right.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Scrollbars from 'react-custom-scrollbars';
+import {Scrollbars} from 'react-custom-scrollbars-2';
 import {Badge} from 'react-bootstrap';
 
 export function renderView(props) {
