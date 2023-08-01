167a717070401409d75ac8a92e6fb6f6b108db18
Nick Misasi <nick.misasi@mattermost.com>
2023-08-01 15:20:33 -0400

    Add files

---
+++ webapp/src/components/sidebar_left/index.js
@@ -0,0 +1,27 @@
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {getCurrentUserId} from 'mattermost-redux/selectors/entities/common';
+
+import {getCloudUserData} from '../../actions';
+
+import {getShowRHSAction, installsForUser} from '../../selectors';
+import SidebarLeft from './sidebar_left'
+
+function mapStateToProps(state) {
+    const id = getCurrentUserId(state);
+    return {
+        id,
+        installs: installsForUser(state, id),
+        showRHS: getShowRHSAction(state),
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators({
+            getCloudUserData,
+        }, dispatch),
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SidebarLeft);
---
+++ webapp/src/components/sidebar_left/sidebar_left.jsx
@@ -0,0 +1,60 @@
+
+
+import React, {useEffect} from 'react';
+
+import PropTypes from 'prop-types';
+import {makeStyleFromTheme, changeOpacity} from 'mattermost-redux/utils/theme_utils';
+import {Tooltip, OverlayTrigger} from 'react-bootstrap';
+
+
+const SidebarLeft = ({ id, installs, actions, showRHS, theme }) => {
+    useEffect(() => {
+        actions.getCloudUserData(id);
+    }, [id, actions]);
+    
+    const style = getStyle(theme);
+
+    return (
+        <div className='CloudPlugin__SidebarLeft'>
+            <OverlayTrigger
+                key='githubYourPrsLink'
+                placement={'right'}
+                
+                overlay={<Tooltip id='yourCloudInstallationsToolTip'>{'Your Cloud installations'}</Tooltip>}
+            >
+                <a
+                    data-testid={'yourCloudInstallationsTestId'}
+                    style={style}
+                    onClick={() => {
+                        console.log("Hey");
+                        showRHS(true);
+                    }}
+                >
+                    <i className='fa fa-cloud' />
+                    {' ' + installs.length}
+                </a>
+            </OverlayTrigger>
+        </div>
+    );
+};
+
+SidebarLeft.propTypes = {
+    id: PropTypes.string.isRequired,
+    installs: PropTypes.array.isRequired,
+    actions: PropTypes.shape({
+        getCloudUserData: PropTypes.func.isRequired,
+    }),
+    theme: PropTypes.object.isRequired,
+    showRHS: PropTypes.func.isRequired,
+}
+
+const getStyle = makeStyleFromTheme((theme) => {
+    return {
+        color: changeOpacity(theme.sidebarText, 0.6),
+        display: 'block',
+        marginBottom: '10px',
+        width: '100%',
+    };
+});
+
+export default SidebarLeft;
