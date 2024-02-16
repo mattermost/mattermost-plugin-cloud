8a95c267fe60831f5bf3db2ca778c0ae66db2d60
Jesse Hallam <jesse.hallam@gmail.com>
2024-02-16 11:45:11 -0400

    allow restarting shared installations

--- server/command_restart.go
+++ server/command_restart.go
@@ -6,23 +6,42 @@ import (
 	cloud "github.com/mattermost/mattermost-cloud/model"
 	"github.com/mattermost/mattermost-server/v6/model"
 	"github.com/pkg/errors"
+	flag "github.com/spf13/pflag"
 )
+
+func getRestartFlagSet() *flag.FlagSet {
+	restartFlagSet := flag.NewFlagSet("restart", flag.ContinueOnError)
+	restartFlagSet.Bool("shared-installation", false, "Set this to true when attempting to restart a shared installation")
+
+	return restartFlagSet
+}
 
 func (p *Plugin) runRestartCommand(args []string, extra *model.CommandArgs) (*model.CommandResponse, bool, error) {
 	if len(args) == 0 || len(args[0]) == 0 {
 		return nil, true, errors.New("must provide an installation name")
 	}
 
+	restartFlagSet := getRestartFlagSet()
+	err := restartFlagSet.Parse(args)
+	if err != nil {
+		return nil, true, err
+	}
+
+	includeShared, err := restartFlagSet.GetBool("shared-installation")
+	if err != nil {
+		return nil, false, err
+	}
+
 	name := standardizeName(args[0])
 
-	installs, _, err := p.getInstallations()
+	installs, err := p.getUpdatableInstallationsForUser(extra.UserId, includeShared)
 	if err != nil {
 		return nil, false, err
 	}
 
 	var installToRestart *Installation
 	for _, install := range installs {
-		if install.OwnerID == extra.UserId && standardizeName(install.Name) == name {
+		if standardizeName(install.Name) == name {
 			installToRestart = install
 			break
 		}
